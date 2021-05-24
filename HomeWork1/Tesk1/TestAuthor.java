0a8841074ac1ff15528cb286e1ae2741fd6ed0a8
GevorgMkhitaryan <59876479+GevorgMkhitaryan@users.noreply.github.com>
2021-05-24 15:26:41 +0400

    HomeWork OOP

---
+++ HomeWork1/Tesk1/TestAuthor.java
@@ -0,0 +1,8 @@
+package HomeWork1.Tesk1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Poghos", "poghos.@mail.ru");
+        System.out.println(author);
+    }
+}
