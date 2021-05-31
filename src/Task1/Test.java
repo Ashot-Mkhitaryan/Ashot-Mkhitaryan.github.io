01b58c365bd0ed5d9a2aec978b06e966d031dd0a
GevorgMkhitaryan <59876479+GevorgMkhitaryan@users.noreply.github.com>
2021-05-31 22:03:47 +0400

    Lesson8

---
+++ src/Task1/Test.java
@@ -0,0 +1,16 @@
+package Task1;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Person person = new Person("EPH", "Abovyan");
+        Student student = new  Student("Poghos", "Erevan", "dfd", 2019, 520000);
+        Staff staff = new Staff("Poghosuhi","Hrazdan","ahaaaa",7);
+
+        System.out.println(person.toString());
+        System.out.println(student.toString());
+        System.out.println(staff.toString());
+
+
+    }
+}
